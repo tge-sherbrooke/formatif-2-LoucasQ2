da8004a247e934df57021ae704587b764baa82fc
LoucasQC2 <viens.louc@hotmail.com>
2026-02-12 10:29:26 -0500

    feat(led): ajout contrôle simple des LEDs

--- led_simple.py
+++ led_simple.py
@@ -22,19 +22,26 @@ LED_VERTE = 27
 LED_JAUNE = 22
 
 # TODO : Configurer le mode BCM
-# GPIO.setmode(GPIO.???)
+GPIO.setmode(GPIO.BCM)
 
 # TODO : Configurer les broches en sortie
-# GPIO.setup(..., GPIO.OUT)
+GPIO.setup([LED_ROUGE, LED_VERTE, LED_JAUNE], GPIO.OUT)
+
 
 def allumer_toutes():
     """Allume toutes les LEDs."""
     # TODO : Implémenter
+    GPIO.output(LED_ROUGE, GPIO.HIGH)
+    GPIO.output(LED_VERTE, GPIO.HIGH)
+    GPIO.output(LED_JAUNE, GPIO.HIGH)
     pass
 
 def eteindre_toutes():
     """Éteint toutes les LEDs."""
     # TODO : Implémenter
+    GPIO.output(LED_ROUGE, GPIO.LOW)
+    GPIO.output(LED_VERTE, GPIO.LOW)
+    GPIO.output(LED_JAUNE, GPIO.LOW)
     pass
 
 def main():
@@ -46,14 +53,18 @@ def main():
     try:
         while True:
             # TODO : Allumer chaque LED une par une
-            # avec 1 seconde d'intervalle
+            print("allumer")
+            allumer_toutes()
+            time.sleep(1)
+            print("éteindre")
+            eteindre_toutes()
             pass
 
     except KeyboardInterrupt:
         print("\nAu revoir!")
     finally:
         # TODO : Nettoyer les GPIO avant de quitter
-        # GPIO.cleanup()
+        GPIO.cleanup()
         pass
 
 if __name__ == "__main__":
